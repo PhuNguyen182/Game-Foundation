17b29f0b51000530a482d1fa91848a42843ee5dc
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-02-11 23:45:56 +0700

    Add new simple UI elements

--- Assets/DracoRuan/Foundation/UISystem/Popups/BaseUIPopup.cs
+++ Assets/DracoRuan/Foundation/UISystem/Popups/BaseUIPopup.cs
@@ -1,9 +1,28 @@
+using DracoRuan.Foundation.UISystem.UIElements;
 using DracoRuan.Foundation.UISystem.Views;
+using UnityEngine;
 
 namespace DracoRuan.Foundation.UISystem.Popups
 {
     public abstract class BaseUIPopup : BaseUIView
     {
-        
+        [SerializeField] protected BaseUIButton closeButton;
+
+        protected virtual void Awake()
+        {
+            if (this.closeButton)
+                this.closeButton.OnClick += this.OnCloseButtonClicked;
+        }
+
+        protected virtual void OnCloseButtonClicked()
+        {
+            this.Hide();
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (this.closeButton)
+                this.closeButton.OnClick -= this.OnCloseButtonClicked;
+        }
     }
 }
