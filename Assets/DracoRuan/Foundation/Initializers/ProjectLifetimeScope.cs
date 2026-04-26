9eaaaea68e394495783ea780d1425edba0e5832e
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-04-26 15:21:54 +0700

    Add VContainer source generator and ProjectLifetimeScope initializers

---
+++ Assets/DracoRuan/Foundation/Initializers/ProjectLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace DracoRuan.Foundation.Initializers
+{
+    public class ProjectLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
