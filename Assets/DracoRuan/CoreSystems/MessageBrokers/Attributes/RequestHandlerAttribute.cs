f5a86f8aa1499d81848345ca749ba26ad2e1ba1a
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-05-04 21:02:52 +0700

    Add new attributes for message handlers and filters

---
+++ Assets/DracoRuan/CoreSystems/MessageBrokers/Attributes/RequestHandlerAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DracoRuan.CoreSystems.MessageBrokers.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+    public class RequestHandlerAttribute : Attribute
+    {
+        
+    }
+}
