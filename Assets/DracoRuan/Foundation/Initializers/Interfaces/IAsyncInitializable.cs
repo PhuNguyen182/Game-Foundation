b6b829f6360a88d13c2f5d154d6f5bb988dc11ef
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-06-24 11:35:11 +0700

    Add comment for consistency with IAsyncInitializable

--- Assets/DracoRuan/Foundation/Initializers/Interfaces/IAsyncInitializable.cs
+++ Assets/DracoRuan/Foundation/Initializers/Interfaces/IAsyncInitializable.cs
@@ -3,7 +3,9 @@
     public interface IAsyncInitializable
     {
         /// <summary>
-        /// Check if this service has been initialized complete yet
+        /// Check if this service has been initialized complete yet,
+        /// All services MUST inherit this interface for waiting in the located lifetime scope consistently.
+        /// Which services inherit this interface should be registered as EntryPoint
         /// </summary>
         public bool IsInitialized();
     }
